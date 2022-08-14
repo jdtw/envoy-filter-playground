57d5805a24baf1b0cc9586b1e2e04a0f2071ee83
John Wood <j@jdtw.us>
2022-08-13 17:56:54 -0700

    Play with http response bodies and dispatch

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::time::Duration;
 
 proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
@@ -16,9 +17,37 @@ struct Config {
 
 struct Filter {
     config: Config,
+    body: Option<String>,
 }
 
-impl Context for Filter {}
+impl Context for Filter {
+    fn on_http_call_response(
+        &mut self,
+        _token_id: u32,
+        _num_headers: usize,
+        body_size: usize,
+        _num_trailers: usize,
+    ) {
+        for (k, v) in self.get_http_call_response_headers() {
+            info!("- {}: {}", k, v);
+        }
+        let headers = self.get_http_call_response_headers();
+        let headers = headers
+            .iter()
+            .map(|(k, v)| (k.as_str(), v.as_str()))
+            .collect();
+        let body = self.get_http_call_response_body(0, body_size);
+        let body = body.as_ref().map(|bs| bs.as_slice());
+        self.send_http_response(200, headers, body)
+    }
+}
+
+enum Do {
+    Fail,
+    Redirect(String),
+    Body(String),
+    Httpbin(String),
+}
 
 impl HttpContext for Filter {
     fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
@@ -26,29 +55,76 @@ impl HttpContext for Filter {
         for (k, v) in &self.config.headers {
             self.set_http_request_header(k.as_str(), Some(v.as_str()))
         }
+
         // Dump all request headers...
-        let mut fail = false;
+        let mut action = None;
         for (k, v) in self.get_http_request_headers() {
             info!("> {}: {}", k, v);
-            if k == "x-fail" {
-                fail = true;
+            match k.as_str() {
+                "x-fail" => action = Some(Do::Fail),
+                "x-redirect" => action = Some(Do::Redirect(v)),
+                "x-body" => action = Some(Do::Body(v)),
+                "x-httpbin" => action = Some(Do::Httpbin(v)),
+                _ => (),
             }
         }
-        // Fail the request if the x-fail header is present...
-        if fail {
-            self.send_http_response(
-                403,
-                vec![("Powered-By", "proxy-wasm")],
-                Some(b"Access forbidden.\n"),
-            );
-            return Action::Pause;
+
+        // Take an action based on the request headers...
+        match action {
+            Some(Do::Fail) => {
+                self.send_http_response(
+                    403,
+                    vec![("Powered-By", "proxy-wasm")],
+                    Some(b"Access forbidden.\n"),
+                );
+                Action::Pause
+            }
+            Some(Do::Redirect(loc)) => {
+                self.send_http_response(302, vec![("Location", &loc)], None);
+                Action::Pause
+            }
+            Some(Do::Body(body)) => {
+                self.body = Some(body);
+                Action::Continue
+            }
+            Some(Do::Httpbin(path)) => {
+                if let Err(e) = self.dispatch_http_call(
+                    "httpbin",
+                    vec![
+                        (":method", "GET"),
+                        (":path", &path),
+                        (":authority", "httpbin.org"),
+                    ],
+                    None,
+                    Vec::new(),
+                    Duration::from_secs(5),
+                ) {
+                    error!("Failed to dispatch: {:?}", e);
+                    self.send_http_response(500, Vec::new(), None)
+                }
+                Action::Pause
+            }
+            None => Action::Continue,
+        }
+    }
+
+    fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
+        if self.body.is_some() {
+            self.set_http_response_header("Content-Length", None);
+            self.set_http_response_header("Content-Type", Some("text/plain"));
+        }
+        for (k, v) in self.get_http_response_headers() {
+            info!("< {}: {}", k, v)
         }
         Action::Continue
     }
 
-    fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
-        for (k, v) in self.get_http_response_headers() {
-            info!("< {}: {}", k, v)
+    fn on_http_response_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
+        if let Some(body) = self.body.as_ref() {
+            if !end_of_stream {
+                return Action::Pause;
+            }
+            self.set_http_response_body(0, body_size, body.as_bytes());
         }
         Action::Continue
     }
@@ -65,7 +141,7 @@ impl RootContext for FilterRoot {
     fn on_configure(&mut self, _plugin_configuration_size: usize) -> bool {
         match self.get_plugin_configuration() {
             Some(bs) => {
-                self.config = serde_json::from_slice(&bs).unwrap();
+                self.config = serde_json::from_slice(&bs).expect("invalid JSON");
                 info!("Loaded config {:?}", self.config);
                 true
             }
@@ -76,9 +152,10 @@ impl RootContext for FilterRoot {
         }
     }
 
-    fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
         Some(Box::new(Filter {
             config: self.config.clone(),
+            body: None,
         }))
     }
 
